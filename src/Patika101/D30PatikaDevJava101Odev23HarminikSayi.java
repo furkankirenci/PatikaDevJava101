f294ba7648d3b984740e75eff3d406d4bc2e24c4
furkankirenci <kirencifurkan1@gmail.com>
2022-05-27 21:46:31 +0300

    harmonik sayı dizisi

---
+++ src/Patika101/D30PatikaDevJava101Odev23HarminikSayi.java
@@ -0,0 +1,19 @@
+package Patika101;
+
+import java.util.Scanner;
+
+public class D30PatikaDevJava101Odev23HarminikSayi {
+    public static void main(String[] args) {
+        // (1/n) + (1/(n+1) + .... şeklindeki harmonik işlemin sonucunu hesaplıyoruz.
+        Scanner inp = new Scanner(System.in);
+
+        double num , total = 0;
+        System.out.println("sayi gir : ");
+        num = inp.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            total += (1.0 / i);
+        }
+        System.out.println(total);
+    }
+}
