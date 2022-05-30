b6177a0e531d38c6d740bb279eb28e087927cb5b
furkankirenci <kirencifurkan1@gmail.com>
2022-05-30 22:05:10 +0300

    Fibonacci serisi

---
+++ src/Patika101/D38PatikaDevJava101Odev31FibonacciSerisi.java
@@ -0,0 +1,29 @@
+package Patika101;
+
+import java.util.Scanner;
+
+public class D38PatikaDevJava101Odev31FibonacciSerisi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kac elemanli bir seri olacak : ");
+        int basNum = input.nextInt();
+        int toplam = 0;
+        int a = 1, b = 0;
+        if (basNum == 0){
+            System.out.println(b);
+        }else if(basNum == 2) {
+            System.out.println(b);
+            System.out.println(a);
+        }else {
+            System.out.println(b);
+            System.out.println(a);
+            int lastToplam = a+b;
+            for (int i = 3; i <= basNum; i++) {
+                toplam = a+b;
+                b=a;
+                a=toplam;
+                System.out.println(toplam);
+            }
+        }
+    }
+}
