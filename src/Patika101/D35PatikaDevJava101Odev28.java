5bf6f984a42bfdc1f02c1d7767d1674bf1daeec3
furkankirenci <kirencifurkan1@gmail.com>
2022-05-30 16:25:17 +0300

    Mukemmel sayı

---
+++ src/Patika101/D35PatikaDevJava101Odev28.java
@@ -0,0 +1,26 @@
+package Patika101;
+
+import java.util.Scanner;
+
+public class D35PatikaDevJava101Odev28 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz : ");
+        int num = inp.nextInt();
+        int i = 1;
+        int toplam = 0;
+        while (i < num) {
+            if (num % i == 0) {
+                toplam += i;
+                i++;
+            } else {
+                ++i;
+            }
+        }
+        if (toplam == num){
+            System.out.println(num+" bir mukemmel sayidir.");
+        }else {
+            System.out.println(num+" bir mukemmel sayi degildir.");
+        }
+    }
+}
