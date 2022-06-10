44691f2b51df3d2a96bc2dd004340d97cf1cd574
Furkan Kirenci <105816790+furkankirenci@users.noreply.github.com>
2022-06-10 06:50:26 +0300

    Update D49PatikaDevJava101Odev40DizilerdeYakinlik.java

--- src/Patika101/D49PatikaDevJava101Odev40DizilerdeYakinlik.java
+++ src/Patika101/D49PatikaDevJava101Odev40DizilerdeYakinlik.java
@@ -8,7 +8,7 @@ public class D49PatikaDevJava101Odev40DizilerdeYakinlik {
         Scanner input = new Scanner(System.in);
         System.out.print("Bir sayi giriniz : ");
         int n = input.nextInt();
-        int[] list = {10, 30, 50, 60, 40, 20};
+        int[] list = {10, 30, 50, 60, 40, 20, -2, -62, -459, 425, 100};
         int max = list[0];
         int min = list[0];
         Arrays.sort(list);
